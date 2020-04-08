09eaf4b907a7fabd5709d1bbc186ded5639421d8
Nusendra Hanggarawan <admin@nusendra.com>
2020-04-08 16:35:43 +0700

    refactor: removes the mysql and sequelize

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,42 +1,41 @@
-import { Request, Response } from "express";
-import Authentication from "../utils/Authentication";
-const db = require("../db/models");
+import { Request, Response } from 'express';
+import Authentication from '../utils/Authentication';
 
 class AuthController {
-    register = async (req: Request, res: Response): Promise<Response> => {
-        let { username, password } = req.body;
-        const hashedPassword: string = await Authentication.passwordHash(password);
+  register = async (req: Request, res: Response): Promise<Response> => {
+    let { username, password } = req.body;
+    const hashedPassword: string = await Authentication.passwordHash(password);
 
-        await db.user.create({ username, password: hashedPassword });
+    // await db.user.create({ username, password: hashedPassword });
 
-        return res.send("registrasi sukses !!");
-    }
+    return res.send('registrasi sukses !!');
+  };
 
-    login = async (req: Request, res: Response): Promise<Response> => {
-        // cari data user by username
-        let { username, password } = req.body;
+  login = async (req: Request, res: Response): Promise<Response> => {
+    // cari data user by username
+    let { username, password } = req.body;
 
-        const user = await db.user.findOne({
-            where: { username }
-        });
+    // const user = await db.user.findOne({
+    //     where: { username }
+    // });
 
-        // check password
-        let compare = await Authentication.passwordCompare(password, user.password);
+    // check password
+    // let compare = await Authentication.passwordCompare(password, user.password);
 
-        // generate token
-        if (compare) {
-            let token = Authentication.generateToken(user.id, username, user.password);
-            return res.send({
-                token
-            });
-        }
+    // generate token
+    // if (compare) {
+    //     let token = Authentication.generateToken(user.id, username, user.password);
+    //     return res.send({
+    //         token
+    //     });
+    // }
 
-        return res.send("auth failed");
-    }
+    return res.send('auth failed');
+  };
 
-    profile = (req: Request, res: Response): Response => {
-        return res.send(req.app.locals.credential);
-    }
+  profile = (req: Request, res: Response): Response => {
+    return res.send(req.app.locals.credential);
+  };
 }
 
 export default new AuthController();
--- src/services/TodoService.ts
+++ src/services/TodoService.ts
@@ -1,10 +1,9 @@
-import { Request } from "express";
-const db = require("../db/models");
+import { Request } from 'express';
 
 class TodoService {
     credential: {
-        id: number,
-        username: string
+        id: number;
+        username: string;
     };
     body: Request['body'];
     params: Request['params'];
@@ -16,66 +15,59 @@ class TodoService {
     }
 
     getAll = async () => {
-        const todos = await db.todo.findAll({
-            where: { user_id: this.credential.id },
-            attributes: ['id', 'description']
-        });
-
-        return todos;
-    }
+        // const todos = await db.todo.findAll({
+        //     where: { user_id: this.credential.id },
+        //     attributes: ['id', 'description']
+        // });
+        // return todos;
+    };
 
     store = async () => {
-        const { description } = this.body;
-        
-        const todo = await db.todo.create({
-            user_id: this.credential.id,
-            description
-        });
-
-        return todo;
-    }
+        // const { description } = this.body;
+        //
+        // const todo = await db.todo.create({
+        //     user_id: this.credential.id,
+        //     description
+        // });
+        // return todo;
+    };
 
     getOne = async () => {
-        const { id } = this.params;
-        
-        const todo = await db.todo.findOne({
-            where: {
-                id,
-                user_id: this.credential.id
-            }
-        });
-
-        return todo;
-    }
+        // const { id } = this.params;
+        //
+        // const todo = await db.todo.findOne({
+        //     where: {
+        //         id,
+        //         user_id: this.credential.id
+        //     }
+        // });
+        // return todo;
+    };
 
     update = async () => {
-        const { id } = this.params;
-        const { description } = this.body;
-
-        const todo = await db.todo.update({
-            description
-        }, {
-            where: {
-                id,
-                user_id: this.credential.id
-            }
-        });
-
-        return todo;
-    }
+        // const { id } = this.params;
+        // const { description } = this.body;
+        // const todo = await db.todo.update({
+        //     description
+        // }, {
+        //     where: {
+        //         id,
+        //         user_id: this.credential.id
+        //     }
+        // });
+        // return todo;
+    };
 
     delete = async () => {
-        const { id } = this.params;
-
-        const todo = await db.todo.destroy({
-            where: {
-                id,
-                user_id: this.credential.id
-            }
-        });
-
-        return todo;
-    }
+        // const { id } = this.params;
+        // const todo = await db.todo.destroy({
+        //     where: {
+        //         id,
+        //         user_id: this.credential.id
+        //     }
+        // });
+        // return todo;
+    };
 }
 
 export default TodoService;
